c208bf59d273d8866f3edc4a6f4238d7d4fb0318
takesoo <takesooon@gmail.com>
2023-08-11 19:18:47 +0900

    resolve N+1 when fetching books with users

--- ruby/src/graphql_on_rails/app/graphql/types/book_type.rb
+++ ruby/src/graphql_on_rails/app/graphql/types/book_type.rb
@@ -7,5 +7,10 @@ module Types
     field :user_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :user, UserType, null: false
+
+    def user
+      Loaders::RecordLoader.for(User).load(object.user_id)
+    end
   end
 end
